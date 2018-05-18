9c88eb18db0e4abe8fcd0f7021347cb53dbda9b1
Thorsten Raasch <raasch@mathematik.uni-siegen.de>
2018-05-18 16:25:44 +0200

    added minus() and static clip() routines

--- classes/sequence.m
+++ classes/sequence.m
@@ -1,16 +1,38 @@
 classdef sequence
     % a class for finite, real-valued sequences over the natural numbers
-    properties
+    properties (Access=public)
         elements  % elements of the sequence (sparse N-by-1 array)
     end
+    
     methods (Access=public)
         function obj=sequence(varargin) % constructor
             switch(nargin)
                 case 0
                     obj.elements=sparse(0,1);
                 case 1
-                    obj.elements=sparse(varargin{1})
+                    obj.elements=sequence.clip(sparse(varargin{1}));
             end
         end
+        function res=plus(obj1,obj2)
+            n1=size(obj1.elements,1);
+            n2=size(obj2.elements,1);
+            v=sparse([obj1.elements; sparse(max(n1,n2)-n1,1)]);
+            v(1:n2,1)=v(1:n2,1)+obj2.elements;
+            res=sequence(sequence.clip(v));
+        end
+        function res=minus(obj1,obj2)
+            n1=size(obj1.elements,1);
+            n2=size(obj2.elements,1);
+            v=sparse([obj1.elements; sparse(max(n1,n2)-n1,1)]);
+            v(1:n2,1)=v(1:n2,1)-obj2.elements;
+            res=sequence(sequence.clip(v));
+        end
     end
+    
+    methods (Static)
+        function t=clip(s)
+            [i,j,v]=find(s);
+            t=s(1:max(i),1);
+        end
+    end    
 end
--- unit_tests/test_sequence.m
+++ unit_tests/test_sequence.m
@@ -4,4 +4,7 @@ clear; close all
 
 s=sequence
 t=sequence(42)
-u=sequence([1 0 42]')
+u=sequence([1 0 42 0]')
+
+v=t+u
+w=t-u
